be8b77540e938c2f6de83b83715f088f27529534
Francisco Ruiz A <francisco.ruizaguilar.newsignature@kantar.com>
2021-02-22 11:07:01 +0000

    Trim configuration, baby steps.

--- src/config.py
+++ src/config.py
@@ -1,12 +1,6 @@
 config = {
     "virgil_fs": "/tmp/my_app",
-    "delta-checkpoint-dir": "/tmp/my_app/checkpoint-dirs/from_delta",
-    "eh-checkpoint-dir": "/tmp/my_app/checkpoint-dirs/from_delta",
-    "file-checkpoint-dir": "/tmp/my_app/checkpoint-dirs/from_file",
-    "csv-input-data": "/tmp/my_app/input-data/csv",
-    "delta-input-data": "/tmp/my_app/input-data/delta",
+    "file-checkpoint-dir": "/tmp/my_app/checkpoint-dirs/file",
     "json-input-data": "/tmp/my_app/input-data/json",
-    "csv-output-data": "/tmp/my_app/output-data/csv",
-    "delta-output-data": "/tmp/my_app/output-data/delta",
     "json-output-data": "/tmp/my_app/output-data/json"
 }

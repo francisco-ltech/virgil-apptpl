f347677b6eadac878c3e0e7733ea7115a6d4e250
jared <magrathj@tcd.ie>
2021-03-13 09:19:45 +0000

    added hadoop exe to repo

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import os
+
 config = {
     "virgil_fs": "/tmp/my_app",
     "file-checkpoint-dir": "/tmp/my_app/checkpoint-dirs/file",
@@ -5,5 +7,8 @@ config = {
     "json-output-data": "/tmp/my_app/output-data/json",
     "delta-checkpoint-dir": "/tmp/my_app/checkpoint-dirs/delta",
     "delta-input-data": "/tmp/my_app/input-data/delta",
-    "delta-output-data": "/tmp/my_app/output-data/delta"
+    "delta-output-data": "/tmp/my_app/output-data/delta",
+    "hadoop": "c:/tmp/my_app/src/jobs/hadoop/"
 }
+
+os.environ['HADOOP_HOME'] = config.get("hadoop")
